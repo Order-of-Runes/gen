207c6501670f610939e84e263ab96035e63246c8
ishowrunes <ishworisgod@gmail.com>
2025-08-14 09:50:34 +0545

    Remove example app

--- gexample/android/app/src/main/kotlin/com/example/gexample/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.gexample
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- gexample/lib/features/sample_model.dart
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025 Order of Runes Authors. All rights reserved.
-
-import 'package:gen_annotation/gen_annotation.dart';
-
-@GenModel()
-abstract class $SampleModel {
-  String? name;
-}
